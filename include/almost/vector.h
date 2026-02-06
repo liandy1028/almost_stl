f1406efa0308508f09191476e2b7ef67aae558a4
Andy Li <liandy1028@gmail.com>
2026-02-06 22:06:32 +0000

    initial commit

---
+++ include/almost/vector.h
@@ -0,0 +1,6 @@
+#ifndef _ALMOST_VECTOR_H
+#define _ALMOST_VECTOR_H
+
+
+
+#endif /* _ALMOST_VECTOR_H */
---
+++ tests/test_vector.cpp
@@ -0,0 +1,6 @@
+#include <almost/vector>
+#include <gtest/gtest.h>
+
+TEST(Basic, Sanity) {
+    EXPECT_EQ(1 + 1, 2);
+}
